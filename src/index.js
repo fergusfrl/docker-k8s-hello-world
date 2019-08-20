1842e367280d90cfef9ce5cc55200a88ef251377
Fergus Farrell <fergusf@orionhealth.com>
2019-08-20 17:33:19 +1200

    added blog route

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const path = require('path');
 const app = express();
- 
+
+// Routes
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+app.get('/blog', (req, res) => {
+    res.sendFile(path.join(__dirname + '/blog.html'));
+});
+
+// Server
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Listening at localhost:${PORT}`);
